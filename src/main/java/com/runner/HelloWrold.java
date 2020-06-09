2719c2231ba14a0a88353b948414eda2ba914db2
xhs_glo <xhs_glo@qq.com>
2020-06-09 20:43:06 +0800

    git commit change_redis

---
+++ src/main/java/com/runner/HelloWrold.java
@@ -0,0 +1,12 @@
+package com.runner;
+
+/**
+ * @author runner
+ * @create 2020-06-09 20:40
+ */
+public class HelloWrold {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
